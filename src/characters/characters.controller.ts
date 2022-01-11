2035c43b2290e10e5d185682cf5273a90c755080
muhammedfkaya <furkan@meditopia.com>
2022-01-12 01:22:11 +0300

    fix: characters endpoints order

--- src/characters/characters.controller.ts
+++ src/characters/characters.controller.ts
@@ -14,11 +14,6 @@ export class CharactersController {
     return this.charactersService.getCharacters(page, seed);
   }
 
-  @Get(':id')
-  getCharacterById(@Param('id') characterId: string): Character {
-    return this.charactersService.getCharacterById(characterId);
-  }
-
   @Get('search')
   searchCharacters(
     @Query('query') query: string,
@@ -26,5 +21,10 @@ export class CharactersController {
     @Query('seed') seed: number,
   ): Character[] {
     return this.charactersService.searchCharacters(query, page, seed);
+  }
+
+  @Get(':id')
+  getCharacterById(@Param('id') characterId: string): Character {
+    return this.charactersService.getCharacterById(characterId);
   }
 }
